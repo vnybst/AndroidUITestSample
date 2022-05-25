0836d2de6b65a351f37041aef785d27f2f2d5e3e
Vinay bisht <vnybst25@gmail.com>
2022-05-25 12:26:30 +0530

    Created ui tests

---
+++ app/src/androidTest/java/com/example/androiduitestsample/MainActivityTest.kt
@@ -0,0 +1,26 @@
+package com.example.androiduitestsample
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule var activityTestRule = activityScenarioRule<MainActivity>()
+
+    // Looks for an EditText with id = "R.id.etInput"
+    // Types the text "Hello" into the EditText
+    // Verifies the EditText has text "Hello"
+    @Test
+    fun validateEditText() {
+        onView(withId(R.id.input)).perform(typeText("Hello")).check(matches(withText("Hello")))
+    }
+}
